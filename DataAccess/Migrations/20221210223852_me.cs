77459367ba3209066f8456b369f0ab44fabd0d55
Molly-Sandera <mollyisapony@mail.ru>
2022-12-11 02:40:05 +0400

    small change

---
+++ DataAccess/Migrations/20221210223852_me.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class me : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Features_CarModelId",
+                table: "Features");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Features_CarModelId",
+                table: "Features",
+                column: "CarModelId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Features_CarModelId",
+                table: "Features");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Features_CarModelId",
+                table: "Features",
+                column: "CarModelId");
+        }
+    }
+}
